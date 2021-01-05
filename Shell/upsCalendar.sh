313ee177c60a235da088880b76fee76310a9dd76
JingMatrix <jingmatrix@gmail.com>
2021-01-05 21:50:10 +0100

    filter and more concise

--- Shell/upsCalendar.sh
+++ Shell/upsCalendar.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/zsh
 # dependency:
 # curl to download data
 # jq (https://stedolan.github.io/jq/) to parse json data
@@ -7,8 +7,33 @@
 # for example, you can use:
 # ./upsCalendar.sh "1week"
 set -e
-start=$(date -I)
-end=$(date -d ${1:-"3days"} -I)
-echo "Course from $start to $end"
-echo "DATE & TIME \t\tInformation\n"
-curl -s 'https://edt.univ-tlse3.fr/calendar2/Home/GetCalendarData' --data-raw "start=$start&end=$end&resType=103&calView=month&federationIds%5B%5D=IMAR9CMA&federationIds%5B%5D=IMARACMA&colourScheme=3" | jq '.[] | "\(.start)\t\(.description )" | sub("\r\n\r\n<br />\r\n\r\n";"\n\t\t\t";"g")' -r
+startdate=$(date -I)
+enddate=$(date -d ${1:-"2days"} -I)
+course=(
+	"Holomorphic dynamics in dimension one : some advanced topics. (F. Bertheloot and P.Roesch)"
+	"Topics in Differential Complex Geometry (E. Legendre)"
+	"Hamilton-Jacobi equations for biology (S. Mirrahimi)"
+	"Stability analysis of ODE's or PDE's periodic solutions. Theoretical and numerical aspects. (P. Noble)"
+	"Learning. (J. M. Loubes, B. Laurent-Bonneau)"
+	"Lévy Processes (L. Huang)"
+)
+echo "Course from $startdate to $enddate"
+echo "DATE\tTIME\tInformation\n"
+curl -s 'https://edt.univ-tlse3.fr/calendar2/Home/GetCalendarData' --data-raw "start=$startdate&end=$enddate&resType=103&calView=month&federationIds%5B%5D=IMAR9CMA&federationIds%5B%5D=IMARACMA&colourScheme=3" | jq -r 'sort_by(.start) | .[] | [.start, .description] | @tsv ' | 
+	while IFS=$'\t' read -r time description
+	do 
+		b=$(<<< $description | sed 's/.*Course \([0-9]\).*/\1/p' -n)
+		if [[ -n $b ]]
+		then
+			# limit to only courses you follow
+			if [[ $b < 4 || $b == 5 ]]
+			then
+				echo -n $(date -d $time +"%a\t%H:%M\t")
+				echo $course[$b]
+			fi
+		else
+			echo -n $(date -d $time +"%a\t%H:%M\t")
+			echo "Geometric group theory. (S. Lamy, M. Sablik)"
+		fi
+	done
+
