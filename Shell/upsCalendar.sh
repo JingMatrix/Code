5466099141a972b11caddd5e6cb4f38cf0c29237
JingMatrix <jingmatrix@gmail.com>
2021-01-05 22:58:04 +0100

    add zoom link

--- Shell/upsCalendar.sh
+++ Shell/upsCalendar.sh
@@ -17,11 +17,24 @@ course=(
 	"Learning. (J. M. Loubes, B. Laurent-Bonneau)"
 	"Lévy Processes (L. Huang)"
 )
+zoomlink=(
+	# DON'T change the order, it is hard-coded order
+	# Complex Geometry Tuesday
+	"zoommtg://univ-tlse3-fr.zoom.us/join?action=join&confno=89280020232&pwd=VFlhdFhyUTZZVFdOUnU2UzloTWVldz09"
+	# Complex Geometry Friday
+	"zoommtg://univ-tlse3-fr.zoom.us/join?action=join&confno=83890569409&pwd=UXRFWjRtZ1duYktCSHJ5Y0tiQ1lHZz09"
+	# Reading seminar
+	"zoommtg://univ-tlse3-fr.zoom.us/join?action=join&confno=81923444202&pwd=d2lRVU9nYy9jNTM1dUE3VEJpSTJOZz09"
+	# Holomorphic dynamics
+	"Who knows?"
+	# Learning
+	"zoommtg://zoom.us/join?action=join&confno=94751387445&pwd=aTFkTVcyeUxCcktRSTZwSDlEVTcxdz09"
+)
 echo "Course from $startdate to $enddate"
 echo "DATE\tTIME\tInformation\n"
-curl -s 'https://edt.univ-tlse3.fr/calendar2/Home/GetCalendarData' --data-raw "start=$startdate&end=$enddate&resType=103&calView=month&federationIds%5B%5D=IMAR9CMA&federationIds%5B%5D=IMARACMA&colourScheme=3" | jq -r 'sort_by(.start) | .[] | [.start, .description] | @tsv ' | 
+curl -s 'https://edt.univ-tlse3.fr/calendar2/Home/GetCalendarData' --data-raw "start=$startdate&end=$enddate&resType=103&calView=month&federationIds%5B%5D=IMAR9CMA&federationIds%5B%5D=IMARACMA&colourScheme=3" | jq -r 'sort_by(.start) | .[] | [.start, .description] | @tsv ' |
 	while IFS=$'\t' read -r time description
-	do 
+	do
 		b=$(<<< $description | sed 's/.*Course \([0-9]\).*/\1/p' -n)
 		if [[ -n $b ]]
 		then
@@ -31,9 +44,31 @@ curl -s 'https://edt.univ-tlse3.fr/calendar2/Home/GetCalendarData' --data-raw "s
 				echo -n $(date -d $time +"%a\t%H:%M\t")
 				echo $course[$b]
 			fi
+			if [[ $zoom == 1 ]]
+				# check zoom link
+			then
+				if [[ $b == 2 || $(date +%a)=='mar.' ]]
+				then
+					xdg-open $zoomlink[1]
+				elif [[ $b == 2 ||  $(date +%a)=='ven.' ]]
+				then
+					xdg-open $zoomlink[2]
+				elif [[ $b == 1 || $(date -I) == $(date -I -d $time) ]]
+				then
+					# not sure, we don't the link yet
+					xdg-open $zoomlink[4] || exit
+				elif [[ $b == 5 || $(date -I) == $(date -I -d $time) ]]
+				then
+					xdg-open $zoomlink[5]
+				fi
+			fi
 		else
 			echo -n $(date -d $time +"%a\t%H:%M\t")
 			echo "Geometric group theory. (S. Lamy, M. Sablik)"
+			if [[ $zoom = 1 && $(date -I) == $(date -I -d $time) ]]
+			then
+				xdg-open $zoomlink[3]
+			fi
 		fi
 	done
 
