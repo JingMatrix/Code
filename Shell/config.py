b22f52333dd13b3de9fb331b97ef0d9330b6c863
JingMatrix <jingmatrix@gmail.com>
2021-06-07 16:59:31 +0200

    qb config block

--- Shell/config.py
+++ Shell/config.py
@@ -12,7 +12,7 @@ config.load_autoconfig(False)
 
 config.bind(',p', 'spawn --userscript qute-pass')
 config.bind('<Ctrl+p>', 'config-cycle -t content.proxy socks5://localhost:9050/ none')
-config.bind('<Ctrl+r>', 'config-cycle colors.webpage.darkmode.enabled ;; restart')
+# config.bind('<Ctrl+r>', 'config-cycle colors.webpage.darkmode.enabled ;; restart')
 config.bind('zo', 'set-cmd-text :open file:///home/jing/')
 
 c.zoom.default = '150%'
@@ -20,8 +20,10 @@ c.aliases['mpv'] = 'spawn --userscript view_in_mpv'
 c.content.blocking.adblock.lists = ['https://easylist.to/easylist/easylist.txt',
         'https://easylist.to/easylist/easyprivacy.txt',
         'https://raw.githubusercontent.com/easylist/easylistchina/master/easylistchina.txt',
-        'https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt']
+        'https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt',
+        'file:///home/jing/Documents/Code/Web/adblock.txt']
 c.content.blocking.method = 'both'
+c.content.autoplay = False
 c.content.pdfjs = True
 c.downloads.remove_finished = 3000
 c.fileselect.handler = 'external'
@@ -30,7 +32,7 @@ c.fileselect.multiple_files.command = ['alacritty','-e','nnn','-n','-p','{}']
 c.fileselect.folder.command = ['alacritty','-e','nnn','-n','-p','{}']
 
 c.window.hide_decoration = True
-
+c.colors.webpage.darkmode.enabled = True
 c.fonts.default_size = '12pt'
 c.fonts.web.family.standard = 'Manjari'
 c.fonts.web.size.default = 17
@@ -39,14 +41,13 @@ c.input.insert_mode.auto_load = True
 # c.qt.force_platform = 'wayland'
 c.tabs.show = 'switching'
 c.statusbar.show = 'in-mode'
-c.url.start_pages = 'https://play.google.com/books/ebooks'
+# c.url.start_pages = 'https://play.google.com/books/ebooks'
+c.url.start_pages = 'https://www.poeticous.com/'
 c.url.searchengines['DEFAULT']='https://www.google.com/search?q={}'
 
 config.set('content.javascript.can_access_clipboard', True, 'https://github.com/*')
 config.set('content.autoplay', True, 'https://jingmatrix.github.io/private/chat')
 config.set('content.notifications.enabled', False)
-config.set('content.headers.user_agent', 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
-        'http://apply.csc.edu.cn/csc/main/person/login/index.jsf')
 
 try:
     from qutebrowser.api import message
